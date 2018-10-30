6ca537791f8f51af2a96c37534ce766d811a4820
Javier Mendoza <javier.mendoza@students.makeschool.com>
2018-10-30 15:50:00 -0700

    Users can create reviews

--- app.js
+++ app.js
@@ -8,11 +8,18 @@ mongoose.connect('mongodb://localhost/rotten-potatoes');
 
 const Review = mongoose.model('Review', {
   title: String,
-  movieTitle: String
+  description: String,
+  movieTitle: String,
 });
+
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// The following line must appear AFTER const app = express() and before your routes!
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.get('/', (req, res) => {
 //   res.render('home', { msg: 'Handlebars are Cool!' });
@@ -33,6 +40,21 @@ app.get('/', (req, res) => {
     .catch(err => {
       console.log(err);
     })
+})
+
+// NEW
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
+})
+
+// CREATE
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
 })
 
 app.listen(3000, () => {
